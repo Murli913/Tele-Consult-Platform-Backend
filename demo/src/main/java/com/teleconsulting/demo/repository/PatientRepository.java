d3f000e05caf92c4cd08867ee03c719248a651b1
Murli913 <murlitalreja913@gmail.com>
2024-03-27 18:20:31 +0530

    add findbyphonenumber ,ethod in patientRepository

--- demo/src/main/java/com/teleconsulting/demo/repository/PatientRepository.java
+++ demo/src/main/java/com/teleconsulting/demo/repository/PatientRepository.java
@@ -3,5 +3,9 @@ package com.teleconsulting.demo.repository;
 import com.teleconsulting.demo.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Patient findByPhoneNumber(String phoneNumber);
+    Optional<Patient> findById(Long id);
 }
