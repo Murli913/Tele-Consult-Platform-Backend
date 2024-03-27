bd6438dd90f481b36b7987b4bdeed24bb8941f12
Murli913 <murlitalreja913@gmail.com>
2024-03-27 18:41:02 +0530

    added new file roomjoinrequest file

---
+++ demo/src/main/java/com/teleconsulting/demo/service/RoomJoinRequest.java
@@ -0,0 +1,22 @@
+package com.teleconsulting.demo.service;
+
+public class RoomJoinRequest {
+    private String patientPhoneNumber;
+    private String doctorPhoneNumber;
+
+    public String getPatientPhoneNumber() {
+        return patientPhoneNumber;
+    }
+
+    public void setPatientPhoneNumber(String patientPhoneNumber) {
+        this.patientPhoneNumber = patientPhoneNumber;
+    }
+
+    public String getDoctorPhoneNumber() {
+        return doctorPhoneNumber;
+    }
+
+    public void setDoctorPhoneNumber(String doctorPhoneNumber) {
+        this.doctorPhoneNumber = doctorPhoneNumber;
+}
+}
