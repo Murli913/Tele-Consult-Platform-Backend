277cda6293b12c38e22b4450d5e4c9c2be5ce5ee
Murli913 <murlitalreja913@gmail.com>
2024-03-27 18:30:12 +0530

    created the dto of pdetails and import lombok

---
+++ demo/src/main/java/com/teleconsulting/demo/dto/Pdetails.java
@@ -0,0 +1,10 @@
+package com.teleconsulting.demo.dto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Pdetails {
+    private String name;
+    private String gender;
+}
