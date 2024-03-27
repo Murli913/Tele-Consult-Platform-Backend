2bfbae86aa18b1f01e54a1a323864246edd90c91
Murli913 <murlitalreja913@gmail.com>
2024-03-27 18:22:01 +0530

    add findbyphonenumber method in doctor repository

--- demo/src/main/java/com/teleconsulting/demo/repository/DoctorRepository.java
+++ demo/src/main/java/com/teleconsulting/demo/repository/DoctorRepository.java
@@ -1,11 +1,15 @@
 package com.teleconsulting.demo.repository;
 
 import com.teleconsulting.demo.model.Doctor;
+import com.teleconsulting.demo.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
     List<Doctor> findBySupervisorDoctorId(Long supervisorId);
+    Doctor findByPhoneNumber(String phoneNumber);
+    Optional<Doctor> findById(Long id);
 }
