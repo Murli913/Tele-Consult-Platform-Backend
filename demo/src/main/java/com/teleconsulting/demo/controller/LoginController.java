c76b31d30e765a66c5896db1f900dd5530e523fb
Murli913 <murlitalreja913@gmail.com>
2024-03-27 19:20:50 +0530

    added a new file logincontroller in the controller package

---
+++ demo/src/main/java/com/teleconsulting/demo/controller/LoginController.java
@@ -0,0 +1,10 @@
+package com.teleconsulting.demo.controller;
+
+import com.teleconsulting.demo.service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class LoginController {
+    @Autowired
+    private DoctorService doctorService;
+
+}
